123aca612cca2f31a1f2065b254f602c6b9e6f35
Xu Hanru <xuhanru@Xus-MacBook-Pro.modem>
2020-03-01 19:50:41 +1100

    add route for 404 page

--- src/core/components/not_found/index.jsx
+++ src/core/components/not_found/index.jsx
@@ -1,3 +1,7 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-export const NotFound = () => <h1>This is not found page</h1>
+export const NotFound = () => <p>
+	This is not found page. &nbsp;
+	<Link to= '/'>Go home</Link>
+</p>
