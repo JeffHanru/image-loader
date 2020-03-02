7bdc8d43cb414863cdc3eac147ef7ba73eef8508
Hanru Xu <hanru@snug.com>
2020-03-02 21:04:58 +1100

    update code

--- src/core/components/error_container/index.js
+++ src/core/components/error_container/index.js
@@ -1,28 +1,17 @@
 import React from 'react'
-import {
-  CenterContentContainer
-} from '../../utils/styled_components/conter_content_container'
-import {
-  Alert,
-  AlertTitle
-} from '@material-ui/lab'
+import { CenterContentContainer } from '../../utils/styled_components/conter_content_container'
+import { Alert, AlertTitle } from '@material-ui/lab'
 
 const alertStyle = {
   width: '100%',
 }
 
-const ErrorContainer = ({
-    error
-  }) => ( < CenterContentContainer >
-    <
-    Alert style = {
-      alertStyle
-    }
-    severity = "error" >
-    <
-    AlertTitle > Error < /AlertTitle> {
-    error
-  } < /Alert>  <
-  /CenterContentContainer >)
+const ErrorContainer = ({ error }) => (
+  <CenterContentContainer>
+    <Alert style={alertStyle} severity="error">
+      <AlertTitle> Error </AlertTitle> {error}{' '}
+    </Alert>{' '}
+  </CenterContentContainer>
+)
 
 export default ErrorContainer
